0a74a77c71986023ae45d5104a642f043060b56a
배용호 <yongho.bae@innowireless.com>
2018-11-09 09:30:36 +0900

    angularjs 공식 튜토리얼 10챕터 까지 완료

--- angularjs_tutorial/app/app.module.js
+++ angularjs_tutorial/app/app.module.js
@@ -3,8 +3,12 @@
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
     // ...which depends on the `phoneList` module
-    'phoneList'
+    'ngRoute',
+    'phoneList',
+    'phoneDetail'
 ]);
 
 Import("phone-list/phone-list.module.js");
 Import("phone-list/phone-list.component.js");
+Import("phone-detail/phone-detail.module.js");
+Import("phone-detail/phone-detail.component.js");
--- angularjs_tutorial/e2e-tests/scenarios.js
+++ angularjs_tutorial/e2e-tests/scenarios.js
@@ -1,9 +1,14 @@
 describe('PhoneCat Application', function() {
 
-    describe('phoneList', function() {
+    it('should redirect `index.html` to `index.html#!/phones', function() {
+        browser.get('index.html');
+        expect(browser.getLocationAbsUrl()).toBe('/phones');
+    });
+
+    describe('View: Phone list', function() {
 
         beforeEach(function() {
-            browser.get('index.html');
+            browser.get('index.html#!/phones');
         });
 
         it('should filter the phone list as a user types into the search box', function() {
@@ -52,6 +57,15 @@ describe('PhoneCat Application', function() {
             query.sendKeys('nexus');
             element.all(by.css('.phones li a')).first().click();
             expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
+        });
+
+        describe('View: Phone detail', function() {
+            beforeEach(function() {
+                browser.get('index.html#!/phones/nexus-s');
+            });
+            it('should display the `nexus-s` page', function() {
+                expect(element(by.binding('$ctrl.phone.name')).getText()).toBe('Nexus S');
+            });
         });
 
 
