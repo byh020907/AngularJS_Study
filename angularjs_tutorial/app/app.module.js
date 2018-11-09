4fd408f1792d03101449bf5674a0d114c379e11e
배용호 <yongho.bae@innowireless.com>
2018-11-09 15:02:13 +0900

    튜토리얼 11완료

--- angularjs_tutorial/app/app.module.js
+++ angularjs_tutorial/app/app.module.js
@@ -4,6 +4,7 @@
 angular.module('phonecatApp', [
     // ...which depends on the `phoneList` module
     'ngRoute',
+    'core',
     'phoneList',
     'phoneDetail'
 ]);
@@ -12,3 +13,6 @@ Import("phone-list/phone-list.module.js");
 Import("phone-list/phone-list.component.js");
 Import("phone-detail/phone-detail.module.js");
 Import("phone-detail/phone-detail.component.js");
+
+Import("core/core.module.js");
+Import("core/checkmark/checkmark.module.js");
---
+++ angularjs_tutorial/app/core/core.module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by INNO-2 on 2018-11-09.
+ */
+angular.module('core',[]);
